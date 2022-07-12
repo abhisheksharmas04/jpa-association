25a98ca68e707274ff53ddb5d3553d30a230b20e
DeLL <DeLL@Abhishek>
2022-07-12 20:32:49 +0530

    Project Created for Assocation mapping

---
+++ src/main/java/com/example/demo/OrMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrMappingApplication.class, args);
+	}
+
+}
